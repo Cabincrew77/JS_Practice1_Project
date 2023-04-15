c7e2cda7909f1dd3d538a2ef7a4aa585eec7d2dc
Kirill Shtylev <kirill.shtylev@me.com>
2023-04-15 19:05:43 +0300

    moving JS task2

---
+++ task2/script.js
@@ -0,0 +1,17 @@
+const consolelog = document.querySelector('#consoleLog');
+
+consolelog.addEventListener('click', () => {
+    alert('Пример использования команды console.log');
+});
+
+const Alert = document.querySelector('#alert');
+
+Alert.addEventListener('click', () => {
+    alert('Пример использования команды Alert');
+});
+
+const Prompt = document.querySelector('#prompt');
+
+Prompt.addEventListener('click', () => {
+    alert('Пример использования команды Prompt');
+});
